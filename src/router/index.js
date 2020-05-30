cd16cf2efb1f33661d8afdd4bb3659ffe245f274
jeffreylubowski <jeffreylubowski@gmail.com>
2020-05-30 11:43:51 +0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,16 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  },
+  {
+    path: "/detailpage1",
+    name: "detailpag1",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "detailpag1" */ "../views/DetailPage1.vue")
+  },
 ];
 
 const router = new VueRouter({
